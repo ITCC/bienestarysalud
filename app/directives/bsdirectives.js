7f280436706e005895f56d4355fcacc2bc725405
Cesar Augsuto Tapia Mori <cesar.tapia@itcc.com.pe>
2015-12-13 11:39:04 -0500

    cambios

--- app/directives/bsdirectives.js
+++ app/directives/bsdirectives.js
@@ -21,6 +21,26 @@
       };
     })
 
+    .directive('linkViewMapr', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'app/partials/link_ver_mapa_partial.html',
+        controller: function($scope, ngDialog) {
+            $scope.openNotify = function (data) {
+                $scope.TituloModal = data[1];
+                $scope.Direccion = data[7];
+                var dialog = ngDialog.open({
+                    template: 'app/partials/map_partial.html',
+                    scope: $scope
+                });
+                dialog.closePromise.then(function (data) {
+                    console.log('ngDialog closed' + (data.value === 1 ? ' using the button' : '') + ' and notified by promise: ' + data.id);
+                });
+            };  
+        }
+      };
+    })
+
     var starApp = angular.module('bienestarysalud.directives');
     starApp.controller('StarCtrl', ['$scope', function ($scope) {
         $scope.rating = 0;
