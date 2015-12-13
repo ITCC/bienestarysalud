da5b5f6bbf6b0b18161f06e6355face720383371
criastos <oskar.crisostomo@itcc.com.pe>
2015-12-13 11:01:52 -0500

    TallerController - Changes
    TallerController - Changes

--- app/controllers/tallercontroller.js
+++ app/controllers/tallercontroller.js
@@ -14,7 +14,7 @@
 			$scope.openNotify = function () {
                var dialog = ngDialog.open({
                     template:
-					   '<div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">HTML Comment Box</a> is loading comments...</div>' +
+					   '<div id="HCB_comment_box">Comentarios cargando...</div>' +
 					   '<link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />' +
 					   '<script type="text/javascript" id="hcb"> if (!window.hcb_user) { hcb_user = {}; } (function () { var s = document.createElement("script"), l = hcb_user.PAGE || ("" + window.location), h = "//www.htmlcommentbox.com"; s.setAttribute("type", "text/javascript"); s.setAttribute("src", h + "/jread?page=" + encodeURIComponent(l).replace("+", "%2B") + "&mod=%241%24wq1rdBcg%24yZJYCdRVDCCmBW2CAdoig1" + "&opts=16862&num=10&ts=1448247820352"); if (typeof s != "undefined") document.getElementsByTagName("head")[0].appendChild(s); })(); </script>',
 						plain: true
