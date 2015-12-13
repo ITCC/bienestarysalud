4411d1d344a7e31a7f7459db7b1ec2bedae269ca
rich4dev <ricardosanchezc10@gmail.com>
2015-12-13 06:21:04 -0500

    test
    test

--- app/controllers/restaurantecontroller.js
+++ app/controllers/restaurantecontroller.js
@@ -1,7 +1,9 @@
 (function(){
 
 	angular.module("bienestarysalud.controllers")
-		.controller("RestauranteController", ["$scope", "DatosAbiertosService", function ($scope, datosAbiertosService){
+		.controller("RestauranteController", 
+				["$scope", "DatosAbiertosService", "ngDialog", 
+				function ($scope, datosAbiertosService, ngDialog){
 			$scope.$root.classContainer = "box-container-red"
 			$scope.$parent.TituloHeader = "Restaurantes";
 			$scope.resultDataRestaurante = [];
@@ -9,7 +11,26 @@
 			datosAbiertosService.getRestauranteResult()
 			.then(function(data){
 				$scope.resultDataRestaurante = data;
-			});			
+			});
+
+			// console.log("");
+
+			$scope.openNotify = function (valor) {
+
+               var dialog = ngDialog.open({
+                    template:
+                        '<p>Address:</p><p>' + valor[7] + '</p>' +
+                        '<br/>'+
+                        '<div id="map_canvas" style="width:400px; height:300px;"></div>' +
+                        '<div class="ngdialog-buttons"><button type="button" class="ngdialog-button ngdialog-button-primary" ng-click="closeThisDialog(1)">Close</button></div>',
+                    plain: true
+                });
+                dialog.closePromise.then(function (data) {
+                    console.log('ngDialog closed' + (data.value === 1 ? ' using the button' : '') + ' and notified by promise: ' + data.id);
+                });
+
+
+            };			
 		}]);
 
 })()
