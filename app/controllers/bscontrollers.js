86fa9b78b24fb2dc99a7903ad9a4ee47a115600f
rich4dev <ricardosanchezc10@gmail.com>
2015-12-13 01:31:47 -0500

    Revert "test"
    This reverts commit 359460aa6f9ce70edcb750afc145d2c49c39b744.

--- app/controllers/bscontrollers.js
+++ app/controllers/bscontrollers.js
@@ -17,21 +17,10 @@
 			});
 		}])
 		.controller("CampanaController", ["$scope", "DatosAbiertosService", function ($scope, datosAbiertosService){
-			$scope.$parent.TituloHeader = "Campaña";
-			$scope.resultDataSalud = [];
-			$scope.resultDataVeterinaria = [];
+			$scope.$parent.TituloHeader = "Actividades";
+			$scope.resultDataSalud = [["2","JORNADA INTEGRAL  GRATUITA DE SALUD","FEBRERO","2/7/15","8:30 AM - 1:00 PM","CASA ENCUENTRO VECINAL POLAR","CALLE JUAN MANUEL POLAR 105","Control peso,talla, presión arterial,laboratorio Clínico, medicina, odontología, consejeria enfermería, nutrición,psicología, Oftalmología, audiometría, podología, fisioterapia,","II"],["3","JORNADA INTEGRAL  GRATUITA DE SALUD","FEBRERO","2/28/15","8:30 AM - 1:00 PM","PARQUE ALFONSO UGARTE","AV.  JAVIER PRADO OESTE CUADRA 17","Control peso,talla, presión arterial,laboratorio Clínico, medicina, odontología, consejeria enfermería, nutrición,psicología, oftalmología, audiometría, podología, fisioterapia, Exposición Talleres  OMAPED","I"],["2","JORNADA INTEGRAL  GRATUITA DE SALUD","FEBRERO","2/7/15","8:30 AM - 1:00 PM","CASA ENCUENTRO VECINAL POLAR","CALLE JUAN MANUEL POLAR 105","Control peso,talla, presión arterial,laboratorio Clínico, medicina, odontología, consejeria enfermería, nutrición,psicología, Oftalmología, audiometría, podología, fisioterapia,","II"],["3","JORNADA INTEGRAL  GRATUITA DE SALUD","FEBRERO","2/28/15","8:30 AM - 1:00 PM","PARQUE ALFONSO UGARTE","AV.  JAVIER PRADO OESTE CUADRA 17","Control peso,talla, presión arterial,laboratorio Clínico, medicina, odontología, consejeria enfermería, nutrición,psicología, oftalmología, audiometría, podología, fisioterapia, Exposición Talleres  OMAPED","I"]];
 
-			datosAbiertosService.getSaludResult()
-			.then(function(data){
-				console.log(data);
-				$scope.resultDataSalud = data;
-			});
 			
-			datosAbiertosService.getVeterinariaResult()
-			.then(function(data){
-				console.log(data);
-				$scope.resultDataVeterinaria = data;
-			});
 		}])
 		.controller("TallerController", ["$scope", "DatosAbiertosService", function ($scope, datosAbiertosService){
 			$scope.$parent.TituloHeader = "Actividades";
