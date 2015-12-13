3fe4d1a6c649561ff7988b8b730f049311e98c66
criastos <oskar.crisostomo@itcc.com.pe>
2015-12-13 02:32:15 -0500

    CambiosTallares
    CambiosTallares

--- app/controllers/tallercontroller.js
+++ app/controllers/tallercontroller.js
@@ -2,8 +2,12 @@
 
 	angular.module("bienestarysalud.controllers")
 		.controller("TallerController", ["$scope", "DatosAbiertosService", function ($scope, datosAbiertosService){
-			$scope.$parent.TituloHeader = "Actividades";
+			$scope.$parent.TituloHeader = "Tallares";
+			$scope.resultDataTalleres = [];
 			
-		}]);
-
+			datosAbiertosService.getTallerResult()
+			.then(function(data){
+				$scope.resultDataTalleres = data;
+			});
+		}])
 })()
