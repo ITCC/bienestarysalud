b1d9093f0464d88e4f5a91cc6da6714946fc460f
Cesar Augsuto Tapia Mori <cesar.tapia@itcc.com.pe>
2015-12-13 03:54:15 -0500

    nuevo cambios

--- app/controllers/campanacontroller.js
+++ app/controllers/campanacontroller.js
@@ -2,19 +2,18 @@
 
 	angular.module("bienestarysalud.controllers")
 		.controller("CampanaController", ["$scope", "DatosAbiertosService", function ($scope, datosAbiertosService){
-			$scope.$parent.TituloHeader = "Campaña";
+			$scope.$root.classContainer = "box-container-orange"
+			$scope.$parent.TituloHeader = "Campañas";
 			$scope.resultDataSalud = [];
 			$scope.resultDataVeterinaria = [];
 
 			datosAbiertosService.getSaludResult()
 			.then(function(data){
-				console.log(data);
 				$scope.resultDataSalud = data;
 			});
 			
 			datosAbiertosService.getVeterinariaResult()
 			.then(function(data){
-				console.log(data);
 				$scope.resultDataVeterinaria = data;
 			});			
 		}]);
